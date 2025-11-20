3750838ee45ec5baeffb0e52a225d53eb93076b5
LeonelV13 <leovaldezm99@gmail.com>
2025-11-20 10:25:07 -0300

    Atributos de Inputs

---
+++ atributos de inputs/codigo.js
@@ -0,0 +1,17 @@
+const input = document.querySelector(".input-normal");
+
+input.className;
+
+input.value;
+
+input.type = "range";
+
+input.accept = "image/png";
+
+const input1 = document.querySelector(".input-normal1");
+
+input1.minLength = 4;
+
+input1.placeholder  = "Escriba aqui";
+
+input1.required = " ";
