279e049edae55e66822460555cf046dd8ef3b9c1
LeonelV13 <leovaldezm99@gmail.com>
2025-11-20 10:27:13 -0300

    DOM

---
+++ DOM/codigo.js
@@ -0,0 +1,9 @@
+const titulo = document.querySelector(".titulo");
+titulo.setAttribute("contentEditable","true");
+
+parrafo = document.getElementsByTagName("p");
+document.write(parrafo)
+
+const rangoEtario = document.querySelector(".rangoEtario");
+
+rangoEtario.setAttribute("type","text");
