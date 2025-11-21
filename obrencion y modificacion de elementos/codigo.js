c67c642bc6e4d0999591d420cc7f1f90cfbe108a
LeonelV13 <leovaldezm99@gmail.com>
2025-11-21 11:04:56 -0300

    Elementos
    Creación
    Obtención
    Modificación

---
+++ obrencion y modificacion de elementos/codigo.js
@@ -0,0 +1,10 @@
+const titulo = document.querySelector(".titulo");
+
+let resultado = titulo.textContent;
+alert(resultado);
+
+let resultado1 = titulo.innerHTML;
+alert(resultado1);
+
+let resultado2 = titulo.outerHTML;
+alert(resultado2);
