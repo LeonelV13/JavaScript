c4527f9d020c600782eea842ad59b35555852300
LeonelV13 <leovaldezm99@gmail.com>
2025-11-26 09:28:32 -0300

    Location

---
+++ location/codigo.js
@@ -0,0 +1,11 @@
+const href = window.location.href;
+
+document.write(href);
+
+window.location.hostname(href);
+
+window.location.pathname(href);
+
+window.location.protocol(href);
+
+window.location.assign(href);
