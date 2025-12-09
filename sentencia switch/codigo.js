1d519d677cfe7a89d6f71fb0ad3a6c4fcbe10f8f
LeonelV13 <leovaldezm99@gmail.com>
2025-12-09 11:14:37 -0300

    Switch

---
+++ sentencia switch/codigo.js
@@ -0,0 +1,18 @@
+let expr = "Banana";
+
+switch(expr){
+    case "Banana":
+        console.log("Esta fruta es de color amarillo");
+        alert("Es Banana");
+        break;
+    case "Pera":
+        console.log("Esta fruta es de color verde claro");
+        alert("Es Pera");
+        break;
+    case "Manzana":
+        console.log("Esta fruta es de color rojo");
+        alert("Es Manzana")
+        break;
+    default:
+        alert("No es ninguna");
+}
