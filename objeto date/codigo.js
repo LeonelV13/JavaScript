73bc1702eaddfff790289f3fff73472a7e67916b
LeonelV13 <leovaldezm99@gmail.com>
2026-01-20 10:53:09 -0300

    Objeto Date

---
+++ objeto date/codigo.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const fecha = new Date();
+
+console.log(fecha.getDate());
+//Fecha Actual
+
+console.log(fecha.getDay());
+// Domingo ---> 0
+// Lunes -----> 1
+// Martes ----> 2
+// Miercoles -> 3
+// Jueves ----> 4
+// Viernes ---> 5
+// Sabado ----> 6
+
+console.log(fecha.getMonth());
+// Enero ------> 0
+// Febrero ----> 1
+// Marzo ------> 2
+// Abril ------> 3
+// Mayo -------> 4
+// Junio ------> 5
+// Julio ------> 6
+// Agosto -----> 7
+// Septiembre -> 8
+// Octubre ----> 9
+// Noviembre --> 10
+// Diciembre --> 11
+
+console.log(fecha.getYear());
+// Año actual - 1900
+// Ej: 2026-1900
+
+console.log(fecha.getHours());
+// Hora Actual
+console.log(fecha.getMinutes());
+// Minutos Actuales
+console.log(fecha.getSeconds());
+// Segundos Actuales
