a1b179de7bc87979d2f004060782e4aca66087b3
LeonelV13 <leovaldezm99@gmail.com>
2026-03-04 10:55:35 -0300

    Objeto Navigator

---
+++ objeto navigator/codigo.js
@@ -0,0 +1,52 @@
+"use strict";
+
+//PROPIEDADES
+console.log("AppCodeName: ",navigator.appCodeName);                         console.log("")
+
+console.log("AppName: ",navigator.appName);                                 console.log("")
+
+console.log("AppVersion: ",navigator.appVersion);                           console.log("")
+
+console.log("Connection: ",navigator.connection);                           console.log("")
+
+console.log("Geolocation: ",navigator.geolocation);                         console.log("")
+
+console.log("HardwareConcurrency: ",navigator.hardwareConcurrency);         console.log("")
+
+console.log("Language: ",navigator.language);                               console.log("")
+
+console.log("Languages: ",navigator.languages);                             console.log("")
+
+console.log("MimeTypes: ",navigator.mimeTypes);                             console.log("")
+
+console.log("OnLine: ",navigator.onLine);                                   console.log("")
+
+console.log("UserAgent: ",navigator.userAgent);                             console.log("")
+
+console.log("CookiesEnabled: ",navigator.cookieEnabled);                    console.log("")
+
+console.log("Permissions: ",navigator.permissions);                         console.log("")
+
+console.log("Plataform: ",navigator.platform);                              console.log("")
+
+console.log("Plugins: ",navigator.plugins);                                 console.log("")
+
+console.log("Product: ",navigator.product);                                 console.log("")
+
+console.log("ServiceWorker: ",navigator.serviceWorker);                      
+
+//METODOS
+
+//navigator.getUserMedia();
+
+//navigator.registerContentHandle();
+
+//navigator.registerProtocolHandler();
+
+//navigator.requestMediaKeySystemAccess();
+
+//navigator.sendBeacon();
+
+//navigator.javaEnabled();
+
+//navigator.vibrate();
